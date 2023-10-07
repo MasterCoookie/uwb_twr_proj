9d5551112904547efe2a47449ccc53dded71dcf2
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-07 14:15:33 +0200

    moves poll frame validation to separate method

--- Src/examples/JK_twr_responder/jk_twr_responder.c
+++ Src/examples/JK_twr_responder/jk_twr_responder.c
@@ -9,6 +9,7 @@
 #include <shared_defines.h>
 #include <shared_functions.h>
 #include <example_selection.h>
+#include <stdbool.h>
 
 
 #if defined(JK_TWR_RESPONDER)
@@ -131,6 +132,36 @@ void await_poll_msg(void)
     { };
 }
 
+//TODO - potentially move to common functions
+uint32_t read_poll_frame(void)
+{
+    /* Clear good RX frame event in the DW IC status register. */
+    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
+
+    return dwt_read32bitreg(RX_FINFO_ID) & RXFLEN_MASK;
+}
+
+bool validate_poll_frame(uint32_t frame_len)
+{
+    dwt_readrxdata(rx_buffer, frame_len, 0);
+    if (frame_len <= sizeof(rx_buffer))
+    {
+        rx_buffer[ALL_MSG_SN_IDX] = 0;
+        if (memcmp(rx_buffer, rx_poll_msg, ALL_MSG_COMMON_LEN) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
 /*! ------------------------------------------------------------------------------------------------------------------
  * @fn main()
  *
@@ -152,59 +183,45 @@ int jk_twr_responder(void)
         await_poll_msg();
 
         if (status_reg & SYS_STATUS_RXFCG_BIT_MASK)
-        {
-            uint32_t frame_len;
-
-            /* Clear good RX frame event in the DW IC status register. */
-            dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
-
-            /* A frame has been received, read it into the local buffer. */
-            frame_len = dwt_read32bitreg(RX_FINFO_ID) & RXFLEN_MASK;
-            if (frame_len <= sizeof(rx_buffer))
+        {            
+            if(validate_poll_frame(read_poll_frame()))
             {
-                dwt_readrxdata(rx_buffer, frame_len, 0);
+                
+                uint32_t resp_tx_time;
+                int ret;
 
-                /* Check that the frame is a poll sent by "SS TWR initiator" example.
-                 * As the sequence number field of the frame is not relevant, it is cleared to simplify the validation of the frame. */
-                rx_buffer[ALL_MSG_SN_IDX] = 0;
-                if (memcmp(rx_buffer, rx_poll_msg, ALL_MSG_COMMON_LEN) == 0)
+                /* Retrieve poll reception timestamp. */
+                poll_rx_ts = get_rx_timestamp_u64();
+
+                /* Compute response message transmission time. See NOTE 7 below. */
+                resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS * UUS_TO_DWT_TIME)) >> 8;
+                dwt_setdelayedtrxtime(resp_tx_time);
+
+                /* Response TX timestamp is the transmission time we programmed plus the antenna delay. */
+                resp_tx_ts = (((uint64_t)(resp_tx_time & 0xFFFFFFFEUL)) << 8) + TX_ANT_DLY;
+
+                /* Write all timestamps in the final message. See NOTE 8 below. */
+                resp_msg_set_ts(&tx_resp_msg[RESP_MSG_POLL_RX_TS_IDX], poll_rx_ts);
+                resp_msg_set_ts(&tx_resp_msg[RESP_MSG_RESP_TX_TS_IDX], resp_tx_ts);
+
+                /* Write and send the response message. See NOTE 9 below. */
+                tx_resp_msg[ALL_MSG_SN_IDX] = frame_seq_nb;
+                dwt_writetxdata(sizeof(tx_resp_msg), tx_resp_msg, 0); /* Zero offset in TX buffer. */
+                dwt_writetxfctrl(sizeof(tx_resp_msg), 0, 1); /* Zero offset in TX buffer, ranging. */
+                ret = dwt_starttx(DWT_START_TX_DELAYED);
+
+                /* If dwt_starttx() returns an error, abandon this ranging exchange and proceed to the next one. See NOTE 10 below. */
+                if (ret == DWT_SUCCESS)
                 {
-                    uint32_t resp_tx_time;
-                    int ret;
+                    /* Poll DW IC until TX frame sent event set. See NOTE 6 below. */
+                    while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS_BIT_MASK))
+                    { };
 
-                    /* Retrieve poll reception timestamp. */
-                    poll_rx_ts = get_rx_timestamp_u64();
+                    /* Clear TXFRS event. */
+                    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
 
-                    /* Compute response message transmission time. See NOTE 7 below. */
-                    resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS * UUS_TO_DWT_TIME)) >> 8;
-                    dwt_setdelayedtrxtime(resp_tx_time);
-
-                    /* Response TX timestamp is the transmission time we programmed plus the antenna delay. */
-                    resp_tx_ts = (((uint64_t)(resp_tx_time & 0xFFFFFFFEUL)) << 8) + TX_ANT_DLY;
-
-                    /* Write all timestamps in the final message. See NOTE 8 below. */
-                    resp_msg_set_ts(&tx_resp_msg[RESP_MSG_POLL_RX_TS_IDX], poll_rx_ts);
-                    resp_msg_set_ts(&tx_resp_msg[RESP_MSG_RESP_TX_TS_IDX], resp_tx_ts);
-
-                    /* Write and send the response message. See NOTE 9 below. */
-                    tx_resp_msg[ALL_MSG_SN_IDX] = frame_seq_nb;
-                    dwt_writetxdata(sizeof(tx_resp_msg), tx_resp_msg, 0); /* Zero offset in TX buffer. */
-                    dwt_writetxfctrl(sizeof(tx_resp_msg), 0, 1); /* Zero offset in TX buffer, ranging. */
-                    ret = dwt_starttx(DWT_START_TX_DELAYED);
-
-                    /* If dwt_starttx() returns an error, abandon this ranging exchange and proceed to the next one. See NOTE 10 below. */
-                    if (ret == DWT_SUCCESS)
-                    {
-                        /* Poll DW IC until TX frame sent event set. See NOTE 6 below. */
-                        while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS_BIT_MASK))
-                        { };
-
-                        /* Clear TXFRS event. */
-                        dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
-
-                        /* Increment frame sequence number after transmission of the poll message (modulo 256). */
-                        frame_seq_nb++;
-                    }
+                    /* Increment frame sequence number after transmission of the poll message (modulo 256). */
+                    frame_seq_nb++;
                 }
             }
         }
